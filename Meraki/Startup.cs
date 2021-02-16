669160a912af68d33e00562d25e37c50cc297cdd
3013688230 <pc>
2021-02-16 03:08:09 -0500

    commit por caicedo 16/02/2021

--- Meraki/Startup.cs
+++ Meraki/Startup.cs
@@ -1,6 +1,8 @@
+using Meraki.Entities;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -27,6 +29,8 @@ namespace Meraki
         {
 
             services.AddControllers();
+            services.AddDbContext<MerakiFinalContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("MerakiConnection")));
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Meraki", Version = "v1" });
